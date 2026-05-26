7d2ec8ec0bbf59258ba2aef76dac1a597fa97b50
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-26 15:16:07 -0400

    added database connection and middleware

--- server.js
+++ server.js
@@ -10,8 +10,12 @@ const projectRouter = require('./routes/projectRoutes');
 const taskRouter = require('./routes/taskRoutes');
 
 // DATABASE CONNECTION
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log('MongoDB connected'))
+    .catch(error => console.log(error));
 
 // MIDDLEWARE
+app.use(express.json());
 
 // ROUTES & ROUTE MOUNTS
 app.get("/test", (req, res) => {
