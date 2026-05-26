0aecb566c5eaf171310bb97cd9bb578f918e007b
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-26 15:24:39 -0400

    added route mounts and a backup port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 require('dotenv').config();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 const mongoose = require('mongoose');
 
 const userRouter = require('./routes/userRoutes');
@@ -12,15 +12,19 @@ const taskRouter = require('./routes/taskRoutes');
 // DATABASE CONNECTION
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('MongoDB connected'))
-    .catch(error => console.log(error));
+    .catch(error => console.log(error.message));
 
 // MIDDLEWARE
 app.use(express.json());
 
 // ROUTES & ROUTE MOUNTS
-app.get("/test", (req, res) => {
+app.get("/", (req, res) => {
     res.send("Testing...");
 });
+
+app.use("/api/users", userRouter);
+app.use("/api/projects", projectRouter);
+app.use("/api/tasks", taskRouter);
 
 // PORT
 app.listen(PORT, () => {
