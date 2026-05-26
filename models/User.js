664e01be3c43c8308becc072365bfd672e403060
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-26 15:41:42 -0400

    removed next function

--- models/User.js
+++ models/User.js
@@ -8,13 +8,11 @@ const userSchema = new mongoose.Schema({
 });
 
 // PRE-SAVE HOOK
-userSchema.pre("save", async function (next) {
+userSchema.pre("save", async function () {
     if (this.isNew || this.isModified("password")) {
         const saltRounds = 10;
         this.password = await bcrypt.hash(this.password, saltRounds);
     }
-
-    next();
 });
 
 // PASSWORD COMPARISON
