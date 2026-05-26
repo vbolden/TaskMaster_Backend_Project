a1f88f55b3f7a05cba65f5020fb912eb24c2dc56
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-26 10:05:08 -0400

    created a task schema and exported it

---
+++ models/Task.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const taskSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    status: { type: String, required: true, enum: ['To Do', 'In Progress','Complete'] },
+    project: {
+        type: Schema.Types.ObjectId,
+        ref: "Project",
+        required: true,
+    }
+});
+
+const Task = mongoose.model("Task", taskSchema);
+module.exports = Task;
