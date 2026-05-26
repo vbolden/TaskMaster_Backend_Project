9b5a77f082017089eb93436cbfb21542d37b6430
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-26 09:34:51 -0400

    added a pre save hook using bcrypt and exported user schema

--- models/User.js
+++ models/User.js
@@ -1,7 +1,21 @@
-const mongoose = require('mongoose').Schema();
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
-    username: {type: String, required: true},
-    email: {type: String, unique: true, required: true},
-    password: {type: String, required: true},
+    username: { type: String, required: true },
+    email: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
 });
+
+// PRE-SAVE HOOK
+userSchema.pre("save", async function (next) {
+    if (this.isNew || this.isModified("password")) {
+        const saltRounds = 10;
+        this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+
+    next();
+});
+
+const User = model("User", userSchema);
+module.exports = User;
