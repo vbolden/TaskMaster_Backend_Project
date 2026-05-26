26b911df424bc1342525bedc31700c2a1cbff1f1
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-26 10:49:02 -0400

    added a login endpoint that finds a user by email and compares the given password to the saved password

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,10 @@
 const userRouter = require('express').Router();
 const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const secret = process.env.JWT_SECRET;
 
-// CREATE
+// REGISTER
 userRouter.post("/register", async (req, res) => {
     try {
         const user = await User.findOne({
@@ -22,6 +25,53 @@ userRouter.post("/register", async (req, res) => {
 
     } catch (error) {
         res.status(400).json({ error: error.message });
+    }
+});
+
+// LOGIN
+userRouter.post("/login", async (req, res) => {
+    try {
+        const user = await User.findOne({
+            email: req.body.email
+        });
+
+        // CHECK FOR EXISTING USER
+        if (!user) {
+            return res.status(400).json({
+                error: "Incorrect email or password."
+            });
+        }
+
+        // COMPARE PROVIDED PASSWORD 
+        const correctPassword = await user.isCorrectPassword(req.body.password);
+
+        if (!correctPassword) {
+            return res.status(400).json({
+                error: "Incorrect email or password."
+            });
+        }
+
+        // PAYLOAD
+        const payload = {
+            _id: user._id,
+            username: user.username,
+            email: user.email,
+        };
+
+        // CREATE TOKEN
+        const token = jwt.sign(payload, secret, { expiresIn: '2h' });
+
+        res.json({
+            token,
+            user: {
+                _id: user._id,
+                username: user.username,
+                email: user.email
+            }
+        });
+
+    } catch (error) {
+        res.status(500).json({ error: error.message });
     }
 });
 
