267efc22c73f64e8b1b896952ef45bbbbf867ea4
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-26 09:27:53 -0400

    created a user schema

---
+++ models/User.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose').Schema();
+
+const userSchema = new mongoose.Schema({
+    username: {type: String, required: true},
+    email: {type: String, unique: true, required: true},
+    password: {type: String, required: true},
+});
