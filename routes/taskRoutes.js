29de3c87e3e874ae4edb5fe38895af792711d013
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-26 14:03:23 -0400

    added a post route for creating a task for one project

---
+++ routes/taskRoutes.js
@@ -0,0 +1,32 @@
+const taskRouter = require('express').Router();
+const Task = require('../models/Task');
+const Project = require('../models/Project');
+const authMiddleware = require('../utils/auth');
+
+// CREATE
+taskRouter.post("/:projectId/tasks", authMiddleware, async (req, res) => {
+    try {
+        // FIND PROJECT BY ID
+        const project = await Project.findOne({
+            _id: req.params.projectId,
+            user: req.user._id,
+        });
+
+        // CHECK IF OWNED BY USER
+        if (!project) {
+            return res.status(403).json({
+                message: "Unauthorized or project not found."
+            });
+        }
+
+        // CREATE TASK
+        const task = await Task.create({
+            ...req.body,
+            project: req.params.projectId,
+        });
+
+        res.status(201).json(task);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
